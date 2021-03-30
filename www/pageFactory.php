3f4f6e790c06209cdf1c02bea647ebb2f0d3e3b3
Aragorn52 <pobeda.deda1945@gmail.com>
2021-03-30 18:34:48 +0400

    4

--- www/pageFactory.php
+++ www/pageFactory.php
@@ -10,16 +10,3 @@ class PageFactory{
 		}
 	}
 }
-/*
-class PageFactory{
-		public function getPage($type){
-			
-			switch($type){
-				case "pdf": return new pdf;
-				case "txt": return new txt;
-				case "php": return new php;
-				default: throw new Exception("Error type");
-			}
-		}
-	}
-	*/
